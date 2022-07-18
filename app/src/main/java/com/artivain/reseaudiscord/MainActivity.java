112f839acf50b73a8550e6bbc734befbb3d118c3
Thomas Fournier <thomas@artivain.com>
2022-07-18 12:32:27 -0400

    Now it can boot and github link is working

--- app/src/main/java/com/artivain/reseaudiscord/MainActivity.java
+++ app/src/main/java/com/artivain/reseaudiscord/MainActivity.java
@@ -1,5 +1,7 @@
 package com.artivain.reseaudiscord;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -20,7 +22,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-	private AppBarConfiguration appBarConfiguration;
 	private ActivityMainBinding binding;
 
 	@Override
@@ -31,18 +32,6 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(binding.getRoot());
 
 		setSupportActionBar(binding.toolbar);
-
-		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-		appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-		binding.fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-								.setAction("Action", null).show();
-			}
-		});
 	}
 
 	@Override
@@ -59,18 +48,13 @@ public class MainActivity extends AppCompatActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
+		if (id == R.id.view_on_github) {
+			Uri uri = Uri.parse(getString(R.string.github_url));
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+			startActivity(browserIntent);
 			return true;
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public boolean onSupportNavigateUp() {
-		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-		return NavigationUI.navigateUp(navController, appBarConfiguration)
-						|| super.onSupportNavigateUp();
 	}
 }
